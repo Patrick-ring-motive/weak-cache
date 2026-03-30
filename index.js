c2bd60202b18e80359b23c74c3b43799af6a123f
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-03-30 12:36:47 -0500

    style: js-beautify formatting

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 // nodejs
 
-(async()=>{
+(async () => {
 
-await import('./weak-cache.js');
+  await import('./weak-cache.js');
 
-var execSync = require('child_process').execSync;
+  var execSync = require('child_process').execSync;
 
-
- try {
+  try {
     let cmd = 'git add --all';
     execSync(cmd).toString();
     cmd = 'git commit -m "_"';
-   execSync(cmd).toString();
+    execSync(cmd).toString();
     cmd = 'git push origin main';
-   execSync(cmd).toString();
- } catch (error) {
-    error.status;  // 0 : successful exit, but here in exception it has to be greater than 0
+    execSync(cmd).toString();
+  } catch (error) {
+    error.status; // 0 : successful exit, but here in exception it has to be greater than 0
     error.message; // Holds the message you typically want.
-    error.stderr;  // Holds the stderr output. Use `.toString()`.
-    error.stdout;  // Holds the stdout output. Use `.toString()`.
- }
+    error.stderr; // Holds the stderr output. Use `.toString()`.
+    error.stdout; // Holds the stdout output. Use `.toString()`.
+  }
 
 })();
--- weak-cache.js
+++ weak-cache.js
@@ -1,123 +1,123 @@
 (() => {
-    const WeakRefMap = (() => {
-        const _weakRefMap = new Map();
-        return class WeakRefMap extends Map {
-            get(key) {
-                try {
-                    const ref = _weakRefMap.get(key);
-                    const value = ref?.deref?.();
-                    if (value === undefined) {
-                        _weakRefMap.delete(key);
-                    }
-                    return value;
-                } catch (e) {
-                    console.warn(e, key);
-                }
-            }
-            set(key, value) {
-                try {
-                    _weakRefMap.set(key, new WeakRef(value));
-                } catch (e) {
-                    console.warn(e, key, value);
-                }
-                return this;
-            }
-            delete(key) {
-                try {
-                    return _weakRefMap.delete(key);
-                } catch (e) {
-                    console.warn(e, key);
-                }
-            }
-            has(key) {
-                try {
-                    const value = _weakRefMap.get(key)?.deref?.();
-                    if (value === undefined) {
-                        _weakRefMap.delete(key);
-                        return false;
-                    }
-                    return true;
-                } catch (e) {
-                    console.warn(e, key);
-                    return false;
-                }
-            }
-        }
-    })();
-    const extend = (thisClass, superClass) => {
+  const WeakRefMap = (() => {
+    const _weakRefMap = new Map();
+    return class WeakRefMap extends Map {
+      get(key) {
         try {
-            Object.setPrototypeOf(thisClass, superClass);
-            Object.setPrototypeOf(
-                thisClass.prototype,
-                superClass?.prototype ??
-                superClass?.constructor?.prototype ??
-                superClass
-            );
+          const ref = _weakRefMap.get(key);
+          const value = ref?.deref?.();
+          if (value === undefined) {
+            _weakRefMap.delete(key);
+          }
+          return value;
         } catch (e) {
-            console.warn(e, {
-                thisClass,
-                superClass
-            });
+          console.warn(e, key);
         }
-        return thisClass;
-    };
-    const instanceOf = (x, y) => {
+      }
+      set(key, value) {
         try {
-            return x instanceof y;
-        } catch {
+          _weakRefMap.set(key, new WeakRef(value));
+        } catch (e) {
+          console.warn(e, key, value);
+        }
+        return this;
+      }
+      delete(key) {
+        try {
+          return _weakRefMap.delete(key);
+        } catch (e) {
+          console.warn(e, key);
+        }
+      }
+      has(key) {
+        try {
+          const value = _weakRefMap.get(key)?.deref?.();
+          if (value === undefined) {
+            _weakRefMap.delete(key);
             return false;
-        }
-    };
-    const isValidResponse = x => (x?.status === 200 && !x?.bodyUsed && !x?.body?.locked) || x?.status === 304;
-    const isResponse = x => instanceOf(x, Response) || x?.constructor?.name == 'Response';
-    const isPromise = x => instanceOf(x, Promise) || x?.constructor?.name == 'Promise' || typeof x?.then === 'function';
-    globalThis.WeakCache = new WeakRefMap();
-    const $response = Symbol('*response');
-    const _fetch = fetch;
-    globalThis.fetch = extend(async function fetch() {
-        let request, response;
-        try {
-            request = new Request(...arguments);
-            if (request.method === 'GET') {
-                let cachedResponse = WeakCache.get(request.url);
-                if (cachedResponse) {
-                    request[$response] = cachedResponse;
-                    if (isPromise(cachedResponse)) {
-                        cachedResponse = await cachedResponse;
-                        if (isValidResponse(cachedResponse)) {
-                            WeakCache.set(request.url, cachedResponse);
-                        } else {
-                            WeakCache.delete(request.url);
-                        }
-                    }
-                    try {
-                        response = cachedResponse.clone();
-                        response[$response] = cachedResponse;
-                    } catch {
-                        WeakCache.delete(request.url);
-                    }
-                    console.log('response from cache');
-                } else {
-                    const presponse = _fetch(...arguments);
-                    WeakCache.set(request.url, presponse);
-                    response = await presponse;
-                    if (response.status === 200 && !response.bodyUsed) {
-                        WeakCache.set(request.url, response.clone());
-                    } else {
-                        WeakCache.delete(request.url);
-                    }
-                }
-            }
-            if (!isResponse(response)) {
-                response = await _fetch(...arguments);
-            }
-            return response;
+          }
+          return true;
         } catch (e) {
-            WeakCache.delete(request.url);
-            return new Response(Object.getOwnPropertyNames(e).map(x => `${x} : ${e[x]}`).join('\n'), {
-                status: 500,
-                statusText: `500 Internal Server Error ${e?.message}`.trim()
-            });
+          console.warn(e, key);
+          return false;
         }
-    },_fetch);
+      }
+    }
+  })();
+  const extend = (thisClass, superClass) => {
+    try {
+      Object.setPrototypeOf(thisClass, superClass);
+      Object.setPrototypeOf(
+        thisClass.prototype,
+        superClass?.prototype ??
+        superClass?.constructor?.prototype ??
+        superClass
+      );
+    } catch (e) {
+      console.warn(e, {
+        thisClass,
+        superClass
+      });
+    }
+    return thisClass;
+  };
+  const instanceOf = (x, y) => {
+    try {
+      return x instanceof y;
+    } catch {
+      return false;
+    }
+  };
+  const isValidResponse = x => (x?.status === 200 && !x?.bodyUsed && !x?.body?.locked) || x?.status === 304;
+  const isResponse = x => instanceOf(x, Response) || x?.constructor?.name == 'Response';
+  const isPromise = x => instanceOf(x, Promise) || x?.constructor?.name == 'Promise' || typeof x?.then === 'function';
+  globalThis.WeakCache = new WeakRefMap();
+  const $response = Symbol('*response');
+  const _fetch = fetch;
+  globalThis.fetch = extend(async function fetch() {
+    let request, response;
+    try {
+      request = new Request(...arguments);
+      if (request.method === 'GET') {
+        let cachedResponse = WeakCache.get(request.url);
+        if (cachedResponse) {
+          request[$response] = cachedResponse;
+          if (isPromise(cachedResponse)) {
+            cachedResponse = await cachedResponse;
+            if (isValidResponse(cachedResponse)) {
+              WeakCache.set(request.url, cachedResponse);
+            } else {
+              WeakCache.delete(request.url);
+            }
+          }
+          try {
+            response = cachedResponse.clone();
+            response[$response] = cachedResponse;
+          } catch {
+            WeakCache.delete(request.url);
+          }
+          console.log('response from cache');
+        } else {
+          const presponse = _fetch(...arguments);
+          WeakCache.set(request.url, presponse);
+          response = await presponse;
+          if (response.status === 200 && !response.bodyUsed) {
+            WeakCache.set(request.url, response.clone());
+          } else {
+            WeakCache.delete(request.url);
+          }
+        }
+      }
+      if (!isResponse(response)) {
+        response = await _fetch(...arguments);
+      }
+      return response;
+    } catch (e) {
+      WeakCache.delete(request.url);
+      return new Response(Object.getOwnPropertyNames(e).map(x => `${x} : ${e[x]}`).join('\n'), {
+        status: 500,
+        statusText: `500 Internal Server Error ${e?.message}`.trim()
+      });
+    }
+  }, _fetch);
 })();
